1969c5107d3502c607fdf6f38320b0a904995505
ManiMindScripts <ar7279796@gmail.com>
2025-09-18 10:09:34 -0700

    updated

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,25 @@ import { getToken } from "next-auth/jwt"
 export async function middleware(request: NextRequest) {
     const token = await getToken({req: request })
     const url = request.nextUrl
-  return NextResponse.redirect(new URL('/home', request.url))
+  
+    if(token &&
+      (
+           (url.pathname.startsWith('/sign-in')) ||
+           (url.pathname.startsWith('/sign-up')) ||
+           (url.pathname.startsWith('/verify'))  ||
+           (url.pathname.startsWith('/'))
+     ) ){
+      return NextResponse.redirect(new URL('/dashboard', request.url)) 
+    }
+
+ if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  // ✅ Otherwise, allow
+  return NextResponse.next();
 }
+
  
 export const config = {
   matcher: [ 
