fb7e46fecede97e8fabb4d81de4da4310d05ed7b
ManiMindScripts <ar7279796@gmail.com>
2025-09-28 22:52:01 -0700

    updated

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,56 @@
-import React from 'react'
+"use client"
+import { Message } from "@/models/User"
+import { acceptMessageSchema } from "@/schemas/acceptMessageSchema"
+import { ApiResponse } from "@/types/ApiResponse"
+import { zodResolver } from "@hookform/resolvers/zod"
+import axios, { AxiosError } from "axios"
+import { useSession } from "next-auth/react"
+import { useCallback, useState } from "react"
+import { useForm } from "react-hook-form"
+import { toast } from "sonner"
 
 const page = () => {
+  const [messages, setMessages] = useState<Message[]>([])
+  const [isLoadin, setIsLoading] = useState(false)
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false)
+
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId))
+  }
+  const { data: session } = useSession()
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema)
+  })
+  const { register, watch, setValue } = form
+  const acceptMessages = watch('acceptMessages')
+  const fetchAcceptMessage = useCallback(async () => {
+    setIsSwitchLoading(true)
+    try {
+      const response = await axios.get('/api/accept-message')
+      setValue('acceptMessages', response.data.isAcceptingMessage)
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>
+      toast.success(axiosError.response?.data.message || "Error to fetch message setting")
+    } finally {
+      setIsSwitchLoading(false)
+    }
+  }, [setValue])
+  const fetchMessage = useCallback(async (refresh: boolean = false) => {
+    setIsLoading(true)
+    setIsSwitchLoading(false)
+    try {
+      const response = await axios.get('/api/get-message')
+      setMessages(response.data.message || [])
+      if (refresh) {
+        toast.success("showing newest messages")
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>
+      toast.success(axiosError.response?.data.message || "Error to fetch message setting")
+    }
+    setIsLoading(false)
+    setIsSwitchLoading(false)
+  }, [setIsLoading,setMessages])
   return (
     <div>page</div>
   )
--- src/app/api/delete-message/messageid/route.ts
+++ src/app/api/delete-message/messageid/route.ts
@@ -3,33 +3,48 @@ import UserModel from "@/models/User";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../auth/[...nextauth]/options";
 import { User } from "next-auth";
-import mongoose from "mongoose";
 
-export async function DELETE(request: Request,{params}: {params : {messageid : string} }) {
+
+export async function DELETE(request: Request, { params }: { params: { messageid: string } }) {
     const messageId = params.messageid
     await dbConnect()
 
     const session = await getServerSession(authOptions)
-    const user : User = session?.user
-    if(!session || session.user){
+    const user: User = session?.user
+    if (!session || session.user) {
         return Response.json({
             success: false,
             message: "Not Authenticated"
-        }, { status: 401 } )
+        }, { status: 401 })
     }
     try {
-      const updateRes = await UserModel.updateOne(
-           {_id: user._id} ,
-           {$pull: {messages:{_id: messageId}}} 
+        const updateRes = await UserModel.updateOne(
+            { _id: user._id },
+            { $pull: { messages: { _id: messageId } } }
         )
-        if(updateRes.modifiedCount == 0) {
+        if (updateRes.modifiedCount == 0) {
             return Response.json(
                 {
-                    
-                }
+                    success: false,
+                    message: "Message not founded"
+                },
+                { status: 404 }
             )
         }
+         return Response.json(
+                {
+                    success: true,
+                    message: "Message deleted"
+                },
+                { status: 200 }
+            )
     } catch (error) {
-        
+return Response.json(
+                {
+                    success: false,
+                    message: "Error deleting Message"
+                },
+                { status: 500 }
+            )
     }
 }
