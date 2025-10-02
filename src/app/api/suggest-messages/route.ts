b4dc909826f28c7bc0d746467bb5eb6d3b2b0fb4
ManiMindScripts <ar7279796@gmail.com>
2025-10-01 21:51:55 -0700

    updated

--- src/app/api/suggest-messages/route.ts
+++ src/app/api/suggest-messages/route.ts
@@ -11,7 +11,7 @@ const openai = createOpenAI({
 
 export async function POST() {
   try {
-    const prompt = "Create a short story about a robot and a cat.";
+    const prompt = "Create a list of three open-ended and engaging questions formatted as a single string. Each question should be separate by ‘||’. These questions are for an anonymous social messaging platform, like Qooh. me, and should be suitable for a diverse audience. Avoid personal or sensitive topics, focusing instead on universal themes that encourage friendly interaction. For example, your output should be structured like this: ‘what’s a hobby you’ve recently started? || if you could have dinner with an historical figure, who would it be ?|| what’s a simple thing that makes you happy?’  Ensure the questions are intriguing, foster curiosity, and contribute to a positive and welcoming conversational environment.";
 
     const result = streamText({
       model: openai("gpt-4o"),
